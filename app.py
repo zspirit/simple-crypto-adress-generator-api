3bb8003343b262b5cac8788520b0bf046be5c113
zspirit <zgafaoui@gmail.com>
2023-05-06 13:01:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ docs = FlaskApiSpec(app)
 
 def getBitcoinAdress():
     wallet = Wallet(testnet=True)
-    return wallet.address.testnet.pubaddr1
+    return wallet.address.testnet.pubaddr1, wallet.key.testnet.wif
 
 def getEthereumAdress():
     priv = secrets.token_hex(32)
     private_key = "0x" + priv
     acct = Account.from_key(private_key)
-    return acct.address
+    return acct.address, private_key
 
 #  Restful way of creating APIs through Flask Restful
 
@@ -53,8 +53,8 @@ class CryptoAdressGeneratorAPI(MethodResource, Resource):
     def get(self, id, ):
         address = ''
         try:
-            address = Cryptoaddress.query.get(id)
-            res = address.serialize() if address else "Unable to find object with this ID : " + str(id)
+            address = Cryptoaddress.query.get_or_404(id)
+            res = address.serialize()
 
             return {'status': 'Success',
                     'code': 200,
@@ -64,22 +64,38 @@ class CryptoAdressGeneratorAPI(MethodResource, Resource):
                             'code': 400,
                             'message': str(e)})
 
+@ns.route('/delete/<int:id>')
+class CryptoAdressGeneratorAPI(MethodResource, Resource):
+    @doc(description='Delete Address.', tags=['DELETE', 'ADRESS', 'GENERATOR'])
+    def delete(self, id, ):
+        address = Cryptoaddress.query.get_or_404(id)
+        res = "Address " + str(id) + " was successfully deleted"
+        try:
+            db.session.delete(address)
+            db.session.commit()
+            return {'status': 'Success',
+                    'code': 200,
+                    'address': res}
+        except:
+            return 'There was an issue deleting  address'
+
 @ns.route('/<string:crypto>')
 class CryptoAdressGeneratorAPI(MethodResource, Resource):
     @doc(description='Generate Address.', tags=['POST', 'ADRESS', 'GENERATOR'])
     def post(self, crypto,):
         address = ''
+        pkey = ''
         try:
             match crypto.upper():
                 case 'BTC':
-                    address = getBitcoinAdress()
+                    address, pkey = getBitcoinAdress()
                 case 'ETH':
-                    address = getEthereumAdress()
+                    address, pkey = getEthereumAdress()
                 case default:
                     address = "Unsupported cryoto"
 
             if address != "Unsupported cryoto":
-                new_entry = Cryptoaddress(address=address, crypto=crypto.upper(), private_key="")
+                new_entry = Cryptoaddress(address=address, crypto=crypto.upper(), private_key=pkey)
                 db.session.add(new_entry)
                 db.session.commit()
 
